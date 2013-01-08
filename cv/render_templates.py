54995e2a6693cdf5391c26d120fc5bcc4c41a31d
Yuval Greenfield <yuval@parasight.com>
2013-01-09 00:22:00 +0200

    Rendering of cv's and 404 page.

---
+++ cv/render_templates.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import jinja2
+
+copies = [
+    {
+        'name': 'dropbox',
+        'id': 'bcvo',
+        'logo': 'dropbox.png',
+    },
+    {
+        'name': 'blizzard',
+        'id': 'rohc',
+        'logo': 'blizzard.gif',
+        'logo_style': 'background-color: black;',
+    }
+]
+
+html = open('secret_template.html', 'rb').read()
+template = jinja2.Template(html)
+
+for info in copies:
+    rendered = template.render(**info)
+    fname = '%s.html' % info['id']
+    with open(fname, 'wb') as f:
+        f.write(rendered)
+
+
